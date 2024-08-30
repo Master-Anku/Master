983f6b613168183ae6ab77cafc79cc42688c677a
Ankura <Master-Anku.com>
2024-08-30 19:27:42 +0630

    test git log

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var date = new Date();
 
 time.append(date);
 time.setAttribute("style", "font-size:1rem;color:yellow")
+
+
+
+// function one(anku) {
+//     console.log("I am 1");
+//    setTimeout(anku, 3000); 
+// }
+
+// function two(m) {
+//     console.log(m);
+   
+// }
+//  setTimeout(() => one (() => two("I am 2")), 3000);
+
+var anku = 20;
+var ma = 30;
+var z = anku + ma;
